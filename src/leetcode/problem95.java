5b64628c1652bedc6c2c6037c65ea2b93102e73b
William.Forever <whuzhaozhongyu@foxmail.com>
2020-05-06 23:58:13 +0800

    77

--- src/leetcode/problem95.java
+++ src/leetcode/problem95.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,6 +31,61 @@ public class problem95 {
             /     /       \                 \
            2     1         2                 3
     * */
+    // 迭代法
+    public List<TreeNode> generateTree(int n ) {
+        List<TreeNode> pre = new ArrayList<TreeNode>();
+        if (n == 0) {
+            return pre;
+        }
+        pre.add(null);
+        //每次增加一个数字
+        for (int i = 1; i <= n; i++) {
+            List<TreeNode> cur = new ArrayList<TreeNode>();
+            //遍历之前的所有解
+            for (TreeNode root : pre) {
+                //插入到根节点
+                TreeNode insert = new TreeNode(i);
+                insert.left = root;
+                cur.add(insert);
+                //插入到右孩子，右孩子的右孩子...最多找 n 次孩子
+                for (int j = 0; j <= n; j++) {
+                    TreeNode root_copy = treeCopy(root); //复制当前的树
+                    TreeNode right = root_copy; //找到要插入右孩子的位置
+                    int k = 0;
+                    //遍历 j 次找右孩子
+                    for (; k < j; k++) {
+                        if (right == null)
+                            break;
+                        right = right.right;
+                    }
+                    //到达 null 提前结束
+                    if (right == null)
+                        break;
+                    //保存当前右孩子的位置的子树作为插入节点的左孩子
+                    TreeNode rightTree = right.right;
+                    insert = new TreeNode(i);
+                    right.right = insert; //右孩子是插入的节点
+                    insert.left = rightTree; //插入节点的左孩子更新为插入位置之前的子树
+                    //加入结果中
+                    cur.add(root_copy);
+                }
+            }
+            pre = cur;
+
+        }
+        return pre;
+    }
+
+
+    private TreeNode treeCopy(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.left = treeCopy(root.left);
+        newRoot.right = treeCopy(root.right);
+        return newRoot;
+    }
 
     // 递归法
     public List<TreeNode> generateTrees(int n) {
---
+++ src/leetcode/problem99.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import com.sun.org.apache.xerces.internal.xs.ItemPSVI;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class problem99 {
+    public static void main(String[] args) {
+        Integer [] nums = new Integer[]{3,1,4,null,null,2};
+        TreeNode root = TreeNode.createTreeNode(nums);
+        new problem99().recoverTree(root);
+    }
+
+    // 中序遍历的时候, 数字是严格按从小到大排序
+    public void recoverTree(TreeNode root) {
+        TreeNode[] treeNodes = new TreeNode[3];
+        inorder(root, treeNodes);
+        int temp = treeNodes[0].val;
+        treeNodes[0].val = treeNodes[1].val;
+        treeNodes[1].val = temp;
+    }
+
+    public void inorder (TreeNode root, TreeNode[] treeNodes) {
+        if (root == null) return;
+        inorder(root.left, treeNodes);
+        if (treeNodes[2] != null) {
+            if (treeNodes[2].val > root.val) {
+                if (treeNodes[0] == null) {
+                    treeNodes[0] = treeNodes[2];
+                    treeNodes[1] = root;
+                } else {
+                    treeNodes[1] = root;
+                }
+            }
+        }
+        treeNodes[2] = root;
+        inorder(root.right, treeNodes);
+    }
+}
