ef85aee6bb12d36231b375e8b00459882a89dbb0
zhaozhongyu <zhaozhongyu@xunlei.com>
2021-05-10 21:43:09 +0800

    problem139

---
+++ src/leetcode/java/problem139.java
@@ -0,0 +1,23 @@
+package leetcode.java;
+
+import java.util.List;
+
+public class problem139 {
+    // 01背包问题 https://github.com/CyC2018/CS-Notes/blob/master/notes/Leetcode%20%E9%A2%98%E8%A7%A3%20-%20%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92.md#0-1-%E8%83%8C%E5%8C%85
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // 可以类比于背包问题
+        int n = s.length();
+        // memo[i] 表示 s 中以 i - 1 结尾的字符串是否可被 wordDict 拆分
+        boolean[] memo = new boolean[n + 1];
+        memo[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (memo[j] && wordDict.contains(s.substring(j, i))) {
+                    memo[i] = true;
+                    break;
+                }
+            }
+        }
+        return memo[n];
+    }
+}
