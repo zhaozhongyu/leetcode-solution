4db85dbf75d45e086b64442cf0dba40f8c086fe2
zhaozhongyu <zhaozhongyu@xunlei.com>
2021-05-17 09:25:58 +0800

    problem148.java

---
+++ src/leetcode/java/problem148.java
@@ -0,0 +1,60 @@
+package leetcode.java;
+
+import leetcode.swordToOffer.ListNode;
+
+public class problem148 {
+
+    // 按当前题目的条件, 使用归并排序
+    public ListNode sortList(ListNode head) {
+        head = mergeSort(head);
+        return head;
+    }
+
+
+    public ListNode mergeSort(ListNode node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode slow = node, fast = node.next;
+        // 用快慢指针找到中间点
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode node2 = slow.next;
+        slow.next = null;
+        ListNode node1 = node;
+        node1 = mergeSort(node1);
+        node2 = mergeSort(node2);
+        ListNode ret = merge(node1, node2);
+        return ret;
+    }
+
+    // 合并两节链表
+    public ListNode merge(ListNode node1, ListNode node2) {
+        ListNode node = new ListNode(-1);
+        ListNode root = node;
+        while (node1 != null && node2 != null) {
+            if (node1.val < node2.val) {
+                node.next = node1;
+                node = node.next;
+                node1 = node1.next;
+            } else {
+                node.next = node2;
+                node = node.next;
+                node2 = node2.next;
+            }
+        }
+        if (node1 != null) {
+            node.next = node1;
+        }
+        if (node2 != null) {
+            node.next = node2;
+        }
+        return root.next;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new problem148().sortList(ListNode.createListNode(new int[]{})));
+    }
+}
