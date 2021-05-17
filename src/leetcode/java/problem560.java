658695d4de30171ce46edc8d1b5ce1f10653f5e7
zhaozhongyu <zhaozhongyu@xunlei.com>
2021-05-17 09:45:10 +0800

    problem560.java

--- src/leetcode/java/problem560.java
+++ src/leetcode/java/problem560.java
@@ -14,6 +14,7 @@ public class problem560 {
         for (i = 0; i < nums.length; i++)
         {
             sum += nums[i];
+            // sum 代表整个数组的和, 那么sum-k就代表了0~j之间的某个子数组的和, 如果出现过, 则表示j~i这里的子数组的和为k, 这里的数字不认为为0的原因是要兼容负数存在
             if (hashMap.containsKey(sum - k))   // 表示连续子数组减去连续子数组，必定为连续子数组
             {
                 count += hashMap.get(sum - k);
@@ -24,6 +25,6 @@ public class problem560 {
     }
 
     public static void main(String[] args) {
-        System.out.println(new problem560().subarraySum(new int[]{1,2,3}, 3));
+        System.out.println(new problem560().subarraySum(new int[]{1,2,3,3,2,1,2}, 3));
     }
 }
