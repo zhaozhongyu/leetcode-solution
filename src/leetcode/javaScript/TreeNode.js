d36e360ebed6eb5cacdd4803f93d0743c9ef0b46
zhaozhongyu <zhaozhongyu@xunlei.com>
2021-09-16 10:55:34 +0800

    treenode

--- src/leetcode/javaScript/TreeNode.js
+++ src/leetcode/javaScript/TreeNode.js
@@ -10,11 +10,11 @@ function createTreeNode(nums) {
   let root = new TreeNode(nums[0]);
   let list = [];
   list.push(root);
-  for (int i = 1; i < nums.length; i+=2) {
+  for (let i = 1; i < nums.length; i+=2) {
     let node = list.shift();
     if (nums[i] != null) {
-        node.left = new TreeNode(nums[i]);
-        list.push(node.left);
+      node.left = new TreeNode(nums[i]);
+      list.push(node.left);
     }
     if (i+1 < nums.length && nums[i+1] != null) {
         node.right = new TreeNode(nums[i+1]);
@@ -25,4 +25,5 @@ function createTreeNode(nums) {
 }
 
 module.exports.createTreeNode = createTreeNode
+module.exports.TreeNode = TreeNode
 
